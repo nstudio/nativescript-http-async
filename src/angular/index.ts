e3cb9c4046b8895c9d3f8d91fc8a973a964ff048
Nathan Walker <walkerrunpdx@gmail.com>
2019-06-24 12:04:30 -0700

    fix(http): post / put method handling

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,12 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { TNSBrowserXhr } from './TNSBrowserXhr';
 import { XhrFactory } from '@angular/common/http';
+import { TNSHttpDebugging } from '../http/http-request-common';
+
+const BASE_PROVIDERS = [
+    TNSBrowserXhr,
+    {provide: XhrFactory, useExisting: TNSBrowserXhr}
+];
 
 @NgModule({
-    providers: [
-        TNSBrowserXhr,
-        {provide: XhrFactory, useExisting: TNSBrowserXhr}
-    ]
+    providers: BASE_PROVIDERS
 })
 export class NativeScriptAsyncModule {
+  static forRoot(options: { configuredProviders?: Array<any>; debug?: boolean; }): ModuleWithProviders {
+    if (options.debug) {
+      TNSHttpDebugging.enabled = true;
+    }
+    return {
+      ngModule: NativeScriptAsyncModule,
+      // Allow others to override if they need more control
+      providers: [...BASE_PROVIDERS, ...(options.configuredProviders || [])]
+    };
+  }
+
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: NativeScriptAsyncModule
+  ) {
+    if (parentModule) {
+      throw new Error(`NativeScriptAsyncModule has already been loaded. Import NativeScriptAsyncModule in the AppModule only.`);
+    }
+  }
 }
